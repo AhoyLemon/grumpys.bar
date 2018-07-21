17be4a232866745b72909c87a2e480db71d8dc8b
Lemon <AhoyLemon@users.noreply.github.com>
2018-07-20 21:01:56 -0500

    GEFN! Sleep.

---
+++ pug/partials/deleteme.js
@@ -0,0 +1,5 @@
+const locations = {
+  northeast: { url: "northeast.html", title: "Northeast", hours: "Mon - Fri: 2pm to 2 am<br />Sat & Sun: 11am to 2pm", phone: "(612) 789-7429", streetAddress: "2200 4th St NE", city:"Minneapolis", state:"MN", zip:"55418", map:"https://goo.gl/maps/eSDad6BRep42"  },
+  title: { url: "roseville.html", title: "Roseville", hours: "Sun & Mon: 11AM to midnight<br />Tue - Sat: 11AM to 2AM", phone: "(651) 379-1180", streetAddress: "2801 Snelling Ave", city: "Roseville", state: "MN", zip:"55113", map:"https://goo.gl/maps/xKkYckTq9gy"  },
+  downtown: { url: "downtown.html", title: "Downtown", hours: "Every Day: 11AM to 2AM", phone: "(612) 340-9738", streetAddress:"1111 S Washington Ave", city:"Minneapolis", state:"MN", zip:"55415" }
+}
