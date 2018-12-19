d1649e38ef690b24ecd7bbfa3288c7f62622b31f
Lemon <citrusmirakel@gmail.com>
2018-12-19 15:52:39 -0600

    homeshot in sitemap.

--- site/templates/xmlsitemap.php
+++ site/templates/xmlsitemap.php
@@ -13,18 +13,16 @@ echo '<?xml-stylesheet type="text/xsl" href="' . $site->url() . '/assets/xsl/ped
   xmlns:xhtml="http://www.w3.org/1999/xhtml"
   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-  <?php foreach($pages->index()->visible() as $p): ?>
+  <?php foreach($pages->index() as $p): ?>
 
     <?php 
     if(in_array($p->uri(), $ignore)) {
       // Page is marked ignore.
-    } else if ($p->expire_date()->isNotEmpty() && $p->expire_date() < date("Y-m-d")) {
-      // Page is expired.
-    } else if ($p->publish_date()->isNotEmpty() && $p->publish_date() > date("Y-m-d")) {
-      // Page is Pending.
+    } else if ($p->isInvisible() && !$p->isHomePage()) {
+      // Hide invisible pages (other than the home page)
     } else if ($p->intendedTemplate() == "booze-type" || $p->intendedTemplate() == "menu-section") {
       // Ignore menu items as pages.
-    } else if ($p->intendedTemplate() != "location" && $p->parent()->isInvisible()) {
+    } else if (!$p->isHomePage() && $p->intendedTemplate() != "location" && $p->parent()->isInvisible()) {
       // Ignore pages with invisible parents.
     } else { ?>
       <url>
@@ -32,10 +30,10 @@ echo '<?xml-stylesheet type="text/xsl" href="' . $site->url() . '/assets/xsl/ped
         <lastmod><?php echo $p->modified('c') ?></lastmod>
         <priority><?php echo ($p->isHomePage()) ? 1 : number_format(0.5/$p->depth(), 1) ?></priority>
 
-        <?php if ($p->teaser_pic()->isNotEmpty()) { ?>
+        <?php if ($p->homeshot()->isNotEmpty()) { ?>
           <image:image>
               <image:loc>
-                <?= $p->url() . '/' . $p->teaser_pic(); ?>
+                <?= $p->url() . '/' . $p->homeshot(); ?>
               </image:loc>
           </image:image>
         <?php } ?>
