179f1e9d11ab44b8ed27f91ebf7a95ad809f035a
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2020-03-20 20:13:28 -0500

    COVID update

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="last-modified" content="2018-06-22@13:19:31">
     <link href="https://fonts.googleapis.com/css?family=Oleo+Script:400,700|Signika:400,600,700" rel="stylesheet">
-    <?php echo css('assets/css/grumpys.css?updated=2020-03-19') ?>
+    <?php echo css('assets/css/grumpys.css?updated=2020-03-20T17:44') ?>
 
     <?php snippet('meta') ?>
     
